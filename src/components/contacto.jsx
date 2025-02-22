e8d1998fb5a3410decd388f88626ed8fb09c9f92
Valery-Cabrera <102255771+Valery-Cabrera@users.noreply.github.com>
2025-02-21 21:05:20 -0500

    Update contacto.jsx

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -9,7 +9,7 @@ export const Contacto = () => {
             <p>
                 Si tienes alguna duda comunicate con nuestra wedding planner
             </p>
-            <a href="https://api.whatsapp.com/send?phone=+3246185894&text=Hola%20Andrea,%20tengo%20una%20duda%20sobre%20la%20boda%20de%20Cami%20&%20Cata,%20me%20podr%C3%ADas%20ayudar%20por%20favor?" target="_blank" rel="noopener noreferrer">
+            <a href="https://api.whatsapp.com/send?phone=+3246185894&text=Hola%20Andrea,%20tengo%20una%20duda%20sobre%20la%20boda%20de%20Cami%20y%20Cata,%20me%20podr%C3%ADas%20ayudar%20por%20favor?" target="_blank" rel="noopener noreferrer">
                 <img src={whatsapp} alt="whatsapp" className='img-whatsapp' />
             </a>
         </div>
