44a09ab08b16f633282fd92587053b8990f48e5e
Valery-Cabrera <102255771+Valery-Cabrera@users.noreply.github.com>
2025-09-19 21:25:08 -0500

    Actualizar hora

--- src/components/lugar.jsx
+++ src/components/lugar.jsx
@@ -6,7 +6,7 @@ export const Lugar = () => {
         <div className='container-lugar'>
             <p className='texto-lugar'>Casa Konna</p>
             <p className='texto-lugar'>Santa Marta, Colombia</p>
-            <p className='texto-lugar'>5:00 PM</p>
+            <p className='texto-lugar'>4:30 PM</p>
             <button onClick={() => {
                 window.open('https://maps.app.goo.gl/9KkkEf3S4hF9vxsbA', '_blank');
             }}>
